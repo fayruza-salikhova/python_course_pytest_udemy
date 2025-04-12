9393c142c0b4ce8d0f5fda561b7b1e9f4429750c
biruzka <biruzka@gmail.com>
2025-04-12 20:47:43 +0300

    add django project

--- tests/marks_test.py
+++ tests/marks_test.py
@@ -1,15 +1,19 @@
 import pytest
 
+
 def test_first() -> None:
     assert 1 == 1
+
 
 @pytest.mark.skip
 def test_should_be_skipped() -> None:
     assert 1 == 2
 
-@pytest.mark.skipif(4>1, reason = "Skipped because 4 > 1")
+
+@pytest.mark.skipif(4 > 1, reason="Skipped because 4 > 1")
 def test_should_be_skipped_if() -> None:
     assert 1 == 2
+
 
 @pytest.mark.xfail
 def test_dont_care_if_fails() -> None:
